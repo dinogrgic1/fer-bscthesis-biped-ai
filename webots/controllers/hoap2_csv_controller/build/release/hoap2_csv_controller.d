425228bb16c3d758a4f591ed97c52d0e1cd884f4
dinogrgic1 <dino.grgic1@gmail.com>
2021-05-28 01:30:56 +0200

    Added train datasets afor some basic movements, writing CSV controller

---
+++ webots/controllers/hoap2_csv_controller/build/release/hoap2_csv_controller.d
@@ -0,0 +1,8 @@
+build/release/hoap2_csv_controller.o: hoap2_csv_controller.cpp \
+ /snap/webots/18/usr/share/webots/include/controller/cpp/webots/Emitter.hpp \
+ /snap/webots/18/usr/share/webots/include/controller/cpp/webots/Device.hpp \
+ /snap/webots/18/usr/share/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ /snap/webots/18/usr/share/webots/include/controller/cpp/webots/Motor.hpp \
+ /snap/webots/18/usr/share/webots/include/controller/cpp/webots/PositionSensor.hpp \
+ /snap/webots/18/usr/share/webots/include/controller/cpp/webots/Robot.hpp \
+ /snap/webots/18/usr/share/webots/include/controller/cpp/webots/TouchSensor.hpp
